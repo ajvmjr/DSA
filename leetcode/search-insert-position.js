765761cbd4cce58bd277c5512fbab11f5edddb5d
Alex Junior <alexjvm90@gmail.com>
2025-01-05 18:34:59 -0300

    adds search-insert-position iterative solution

---
+++ leetcode/search-insert-position.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  let i = 0;
+  let j = nums.length - 1;
+
+  while (i <= j) {
+      const mid = Math.floor((i + j) / 2);
+      if (nums[mid] === target) return mid;
+
+      if (target < nums[mid]) {
+          j = mid - 1;
+      } else {
+          i = mid + 1;
+      }
+  }
+
+  return i;
+};
+
+// https://leetcode.com/problems/search-insert-position
