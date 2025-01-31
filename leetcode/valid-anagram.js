7e6b77c9e26a602a7d81bdf838df82049c2c3290
Alex Moreira <alexjvm90@gmail.com>
2025-01-31 18:34:05 -0300

    adds second solution to valid anagram

--- leetcode/valid-anagram.js
+++ leetcode/valid-anagram.js
@@ -3,6 +3,7 @@
  * @param {string} t
  * @return {boolean}
  */
+// Solution 1 - Time O(n) - Space O(n)
 var isAnagram = function (s, t) {
   if (s.length !== t.length) return false;
 
@@ -34,3 +35,22 @@ var isAnagram = function (s, t) {
 
   return true;
 };
+
+// Solution 2 - Time O(n) - Space O(n)
+var isAnagram2 = function (s, t) {
+  if (s.length !== t.length) return false;
+  const count = {};
+
+  for (let i = 0; i < s.length; i++) {
+    count[s[i]] = (count[s[i]] || 0) + 1;
+    count[t[i]] = (count[t[i]] || 0) - 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (count[s[i]] !== 0) return false;
+  }
+
+  return true;
+};
+
+// https://leetcode.com/problems/valid-anagram
