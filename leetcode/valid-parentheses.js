ab49cd4d4c187993747bad67545b0392ae7665ff
Alex Moreira <alexjvm90@gmail.com>
2025-02-05 01:49:40 -0300

    adds more elegant solution to valid-parentheses

--- leetcode/valid-parentheses.js
+++ leetcode/valid-parentheses.js
@@ -32,4 +32,21 @@ var isValid = function (s) {
   return true;
 };
 
+// Solution 2
+// Time O(n) - Space O(n)
+var isValid2 = function (s) {
+  const stack = [];
+  const pairs = { ")": "(", "]": "[", "}": "{" };
+
+  for (const c of s) {
+    if (c in pairs) {
+      if (stack.pop() !== pairs[c]) return false;
+    } else {
+      stack.push(c);
+    }
+  }
+
+  return stack.length === 0;
+};
+
 // https://leetcode.com/problems/valid-parentheses/
