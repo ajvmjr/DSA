d1486a53f4ad6d23459eac6d5803e18d6eaee56c
Alex Moreira <alexjvm90@gmail.com>
2025-03-05 07:46:13 -0300

    adds middle-node solution

---
+++ leetcode/middle-of-linked-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// Time O(n) - Space O(n)
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+};
+
+// https://leetcode.com/problems/middle-of-the-linked-list
