fa896a286b11e2ce2deebf967feb397a0893cab0
Alex Moreira <alexjvm90@gmail.com>
2025-03-05 09:03:22 -0300

    updates maximum-subarray solution

--- leetcode/maximum-subarray.js
+++ leetcode/maximum-subarray.js
@@ -5,7 +5,8 @@
 
 // Time O(n) - Space O(1)
 var maxSubArray = function (nums) {
-  let maxSum = (currentSum = nums[0]);
+  let maxSum = nums[0];
+  let currentSum = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
     if (nums[i] + currentSum <= nums[i]) {
